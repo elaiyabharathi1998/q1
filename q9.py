155ed092d3848a20a566980786e603818553d0c4
elaiyabharathi1998 <41041191+elaiyabharathi1998@users.noreply.github.com>
2018-07-11 12:12:35 +0530

    Create q9.py

---
+++ q9.py
@@ -0,0 +1,33 @@
+N=input()
+
+
+
+sum=0
+
+
+
+if N.isalpha() :
+
+
+
+	print("invalid")
+
+
+
+else:
+
+
+
+	N=int(N)
+
+	
+
+        arr=int(input())
+
+
+
+	sum=sum+a[i]
+
+    
+
+    print(sum)

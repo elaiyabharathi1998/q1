a67285aebfacdfb0f99ad7f7107aefea88391eeb
elaiyabharathi1998 <41041191+elaiyabharathi1998@users.noreply.github.com>
2018-07-11 11:57:12 +0530

    Create q5.py

---
+++ q5.py
@@ -0,0 +1,27 @@
+u=raw_input()
+
+v=raw_input()
+
+w=raw_input()
+
+a=int(u)
+
+b=int(v)
+
+c=int(w)
+
+if((a>=b) and (a>=c)):
+
+   print a
+
+elif(b>=a) and (b>=c):
+
+	print b
+
+elif(c>=a) and (c>=b):	
+
+    print c
+
+else:
+
+   print("invalid")
